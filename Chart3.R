a4997e0ac7b371dae88d69cc415c652ad89ec971
senudi-ds <ssiriw@uw.edu>
2023-02-23 20:05:07 -0800

    chart3 added

---
+++ Chart3.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(plotly)
+library(stringr)
+
+
+library_data <- read.csv("~/Desktop/INFO201/2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
+library_data$Title[str_detect(library_data$Title, "Politics")] <- "Politics"
+
+world_issues_books <- library_data %>% filter(Title == "Politics") %>% group_by(CheckoutYear) %>% summarize(politics_checkouts = max(Checkouts))
+
+ggplot(world_issues_books) +
+  geom_line(mapping = aes(x = CheckoutYear, y = politics_checkouts, color = "Accent")) +
+  labs(title = "The Peak of Politics", x = "Year", y = "Max Checkouts of Books Related to Politics")
--- chart2_example.R
+++ chart2_example.R
@@ -13,12 +13,3 @@ ggplot(physical_checkout) +
        x = "Usage", 
        y = "Total Checkouts") 
 
-
-library(stringr)
-library_data$Title[str_detect(library_data$Title, "Politics")] <- "Politics"
-
-world_issues_books <- library_data %>% filter(Title == "Politics") %>% group_by(CheckoutYear) %>% summarize(politics_checkouts = max(Checkouts))
-
-ggplot(world_issues_books) +
-  geom_line(mapping = aes(x = CheckoutYear, y = politics_checkouts, color = "Accent")) +
-  labs(title = "The Peak of Politics", x = "Year", y = "Max Checkouts of Books Related to Politics")
