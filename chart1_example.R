96c6fd7061eff163a60f254126980d2f0039d4e9
senudi-ds <ssiriw@uw.edu>
2023-02-23 19:59:51 -0800

    A3

--- chart1_example.R
+++ chart1_example.R
@@ -4,9 +4,7 @@ library(stringr)
 
 library_data <- read.csv("~/Desktop/INFO201/2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
 
-library_data$Creator[str_detect(library_data$Creator, "Colleen Hoover")] <- "Colleen Hoover"
-library_data$Title[str_detect(library_data$Title, "It Ends With Us")] <- "It Ends With Us"
-library_data$Title[str_detect(library_data$Title, "")] <- "It Ends With Us"
+library_data$Creator[str_detect(library_data$Creator, 'Colleen Hoover')] <- 'Colleen Hoover'
 
 colleen_books <- library_data %>% filter(Creator == "Colleen Hoover") %>% group_by(PublicationYear) %>% summarize(most_published_book = max(Checkouts))
 
